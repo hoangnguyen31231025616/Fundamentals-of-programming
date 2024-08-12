8032921f81ecd2941559dde5179946c645899de9
hoangnguyen31231025616 <hoangnguyen.31231025616@st.ueh.edu.vn>
2024-08-12 20:04:32 +0700

    bai tap 2

--- bài tập buổi 2/obj/Debug/net8.0/bài tập buổi 2.AssemblyInfo.cs
+++ bài tập buổi 2/obj/Debug/net8.0/bài tập buổi 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bài tập buổi 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114c599b1e33d625bba84b9f3032210d856e4dae")]
 [assembly: System.Reflection.AssemblyProductAttribute("bài tập buổi 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bài tập buổi 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
