d0a5af83be22547884b22019377831ff2d372565
khanhhoang9918 <khanhhoang9918@gmail.com>
2024-08-19 16:10:22 +0700

    hoan thanh bai 1 va 2 baitapString

---
+++ bài tập String/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalCSharp
+{
+    internal class bài_tập_string
+    {
+        public static void Main(string[] args)
+        {
+            //baitap1();
+            //baitap2();
+            baitap3();
+        }
+        static void baitap1()
+        {
+            Console.WriteLine("input string: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("the string you entered is: "+ s);
+        }
+        static void baitap3()
+        {
+            Console.WriteLine("input string: ");
+            string s = Console.ReadLine();
+            var characters = s.Split('');
+            Console.WriteLine(characters);
+        }
+        static void baitap2()
+        {
+            Console.WriteLine("input string: ");
+            string s = Console.ReadLine();
+            int numberOfLetters = 0;
+            foreach (var c in s)
+                {
+                    numberOfLetters++;
+                }
+            Console.WriteLine("Length of string: "+ numberOfLetters);
+        }
+    }
+}           
