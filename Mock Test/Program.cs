b2892efe569e5fda535756cba249dc4cee5f98e8
khanhhoang9918 <khanhhoang9918@gmail.com>
2024-09-29 19:40:59 +0700

    commit .

--- Mock Test/Program.cs
+++ Mock Test/Program.cs
@@ -16,8 +16,97 @@ namespace FundamentalCSharp
     {
         public static void Main(string[] args)
         {
-
+            //Console.Write("nhap so a: \n");
+            //int a = Convert.ToInt16(Console.ReadLine());
+            //Console.Write("nhap so b: \n");
+            //int b = Convert.ToInt16(Console.ReadLine());
+            //int ucln = UCLN(a, b);
+           // int bcnn = BCNN(a, b, ucln);
+            //Console.Write($"UCLN: {ucln} \n");
+            //Console.Write($"BCNN {bcnn}");
+            //Console.Write("Enter a string :");
+            //string str = Console.ReadLine();
+            Console.Write("enter N: \n");
+            int StringLenth = Convert.ToInt16(Console.ReadLine());
+            int[] Numbers = new int[StringLenth];
+            InputArray(Numbers);
+            int sum = SumRevered(Numbers);
+            Console.Write($"sum reversed: {sum}");
         }
-        
+        static int[] InputArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Input number at index {i}: ");
+                bool res = int.TryParse(Console.ReadLine(), out numbers[i]);
+                if (!res)
+                {
+                    Console.Write($"Wrong format, Input number at index {i}: ");
+                    i--;
+                }
+            }
+            return numbers;
+        }
+        static int UCLN(int a, int b)
+        {
+             int UCLN = 0;
+            for (int i = 1; i < a && i < b; i++)
+            {
+                if(a % i == 0 && b % i == 0)
+                {
+                    UCLN = i;
+                }
+            }
+            return UCLN;
+        }
+        static int BCNN(int a, int b, int UCLN)
+        {
+            int BCNN = (a*b)/UCLN; 
+            return BCNN;
+        }
+        static void CountLetterAndNumber(string str)
+        {
+            int SumL = 0;
+            int SumN = 0;
+            int SumS = 0;
+            int Sum = 0;
+            char[] strArray = str.ToCharArray();
+            foreach (var item in strArray)
+            {
+                if (char.IsDigit(item))
+                    SumN++;
+                if (char.IsLetter(item))
+                    SumL++;
+                if (!char.IsLetterOrDigit(item))
+                    SumS++;
+                Sum++;
+            }
+            Console.WriteLine("Total Characters In String : "+Sum);
+            Console.WriteLine("Total Letter Character String : "+SumL);
+            Console.WriteLine("Total Digit Character String : "+SumN);
+            Console.WriteLine("Total Special Character String : "+SumS);
+        }
+        static int reversed_number(int number)
+        {
+            int reverse = 0;
+            while ( number > 0)
+                {
+                    int remainder = number % 10;
+                    reverse = (reverse * 10) + remainder;
+                    number = number / 10;
+                }
+            Console.WriteLine(reverse);
+            return reverse;
+        }
+        static int SumRevered(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] >50 && numbers[i] <100 && numbers[i]%2==1) 
+                sum+=reversed_number(numbers[i]);
+            }
+            return sum;
+        }
     }
 }
--- Mock Test/obj/Debug/net8.0/Mock Test.AssemblyInfo.cs
+++ Mock Test/obj/Debug/net8.0/Mock Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mock Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a3b8dd8a0d76c1676e7bf49310c8a795581272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b108cfe1d38486bd7b023785f6abfc7bb673124f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mock Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mock Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
